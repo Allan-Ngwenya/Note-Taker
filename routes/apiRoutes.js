bf2a3705e17808ad2defc4c2d8c08681bb6c8b98
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-01-23 18:46:49 +0000

    api router delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,3 +16,12 @@ router.post('/notes', (req, res) => {
 		.then((note) => res.json(note))
 		.catch(err => res.status(500).json(err));
 });
+
+router.delete('/notes/:id', function (req, res) {
+	saveData
+		.deleteNote(req.params.id)
+		.then(() => res.json({ ok: true }))
+		.catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
