e4c2cc0fd008ac2edc0ff14ce2dd7bcdb5545298
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-01-23 19:31:29 +0000

    requird variables

---
+++ public/assets/js/index.js
@@ -0,0 +1,29 @@
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
+
+var activeNote = {};
+
+var getNotes = function () {
+	return $.ajax({
+		url: "/api/notes",
+		method: "GET"
+	});
+};
+
+var saveNote = function (note) {
+	return $.ajax({
+		url: "/api/notes",
+		data: note,
+		method: "POST"
+	});
+};
+
+var deleteNote = function (id) {
+	return $.ajax({
+		url: "api/notes/" + id,
+		method: "DELETE"
+	});
+};
