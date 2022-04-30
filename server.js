8080bb85b076cff01db786354d48d057a47a86b9
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-04-30 15:14:53 +0100

    routes added, errors resolcve and save functionality resolved

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
-// Dependencies
-const express = require('express');
+const express = require("express");
 
-
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
-// express server
-const app = express();
-
-// PORT
 const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.urlencoded({ extended: true }));
-
-// JSON data
 app.use(express.json());
-
-
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 app.listen(PORT, () => {
-	console.log(`API server is ready on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
